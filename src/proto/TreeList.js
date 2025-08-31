04a5a8a8ebb67c9f62fd6750fb1f529b334e2791
Federico Marino <fedemarino@gmail.com>
2025-08-31 10:21:25 -0300

    fix: render treeList component

--- src/proto/TreeList.js
+++ src/proto/TreeList.js
@@ -18,9 +18,10 @@ export class TreeList extends Proto {
         this.maxLeaf = computeMaxLeaf(this.tree);
 
         this.c[2] = this.dom('div', this.css.basic + 'top:0; left:0; width:100%;');
+        this.c[2].style.pointerEvents = 'auto';
 
-        this.build();
         this.init();
+        this.build();
     }
 
     setTree(tree) {
@@ -46,16 +47,21 @@ export class TreeList extends Proto {
         let node = this.tree;
         let path = this.focused ? this.focusPath : this.value;
         let level = 0;
+        let total = 0;
 
         while (node) {
             const isLeaf = Array.isArray(node);
             const options = isLeaf ? node : Object.keys(node);
-            const container = this.dom('div', this.css.basic + 'display:flex;' + (isLeaf ? 'flex-direction:column;' : 'flex-direction:row;'));
-            container.style.height = (isLeaf ? this.maxLeaf * this.h : this.h) + 'px';
+            const rowHeight = isLeaf ? this.maxLeaf * this.h : this.h;
+            const container = this.dom('div', 'position:absolute; display:flex;' + (isLeaf ? 'flex-direction:column;' : 'flex-direction:row;'));
+            container.style.top = total + 'px';
+            container.style.height = rowHeight + 'px';
+            container.style.pointerEvents = 'auto';
 
             options.forEach(opt => {
                 const optDiv = this.dom('div', this.css.item + 'margin:1px; line-height:' + (this.h - 4) + 'px;');
                 optDiv.style.height = (this.h - 2) + 'px';
+                optDiv.style.pointerEvents = 'auto';
                 optDiv.textContent = opt;
                 if (this.value[level] === opt) optDiv.style.background = this.colors.select;
                 if (this.focused && this.focusLevel === level && this.focusPath[level] === opt) {
@@ -71,11 +77,13 @@ export class TreeList extends Proto {
                 const diff = this.maxLeaf - options.length;
                 for (let i = 0; i < diff; i++) {
                     const spacer = this.dom('div', this.css.basic + 'height:' + (this.h - 2) + 'px;');
+                    spacer.style.pointerEvents = 'none';
                     container.appendChild(spacer);
                 }
             }
 
             this.c[2].appendChild(container);
+            total += rowHeight;
 
             if (isLeaf) break;
 
@@ -87,6 +95,12 @@ export class TreeList extends Proto {
                 break;
             }
         }
+
+        this.h = total;
+        this.s[0].height = total + 'px';
+        this.s[2].height = total + 'px';
+        this.zone.h = total + this.margin;
+        this.rSize();
     }
 
     select(level, option) {
@@ -106,6 +120,12 @@ export class TreeList extends Proto {
         }
         this.onChange(this.tabIndex, this.itemIndex, newPath);
         this.setValue(newPath);
+    }
+
+    rSize() {
+        super.rSize();
+        this.s[2].left = this.sa + 'px';
+        this.s[2].width = this.sb + 'px';
     }
 }
 
